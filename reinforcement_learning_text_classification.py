bc010f7dedb564ba4530746b558f2bd15784cb8e
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-02-18 23:09:36 +0100

    Create reinforcement_learning_text_classification.py

---
+++ reinforcement_learning_text_classification.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+from tensorflow.keras import layers
+import numpy as np
+import os
+import random
+from autokeras import TextClassifier
+
+# Set up file paths
+MODEL_DIR = "e:\\__results\\gyndroid_model\\"
+TEXT_DATA_FILE = os.path.join(MODEL_DIR, "text_data.npy")
+GPT2_DATA_FILE = os.path.join(MODEL_DIR, "gpt2_data.npy")
+LABELS_FILE = os.path.join(MODEL_DIR, "labels.npy")
+CHECKPOINT_FILE = os.path.join(MODEL_DIR, "checkpoint.ckpt")
+LOG_FILE = os.path.join(MODEL_DIR, "training.log")
+
+# Load and preprocess the data
+with open('path/to/your/input/text/file.txt', 'r') as f:
+    lines = f.readlines()
+text_data = [line.strip() for line in lines]
+labels = [0]*len(text_data)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(text_data, labels, test_size=0.2, random_state=42)
+
+# Define input layer
+text_input = tf.keras.Input(shape=(None,), name='text_input')
+
+# Define LSTM layer
+lstm_layer = layers.LSTM(256, name='lstm_layer')(text_input)
+
+# Define GPT-2 XL model
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-xl")
+gpt2_model = TFGPT2LMHeadModel.from_pretrained("gpt2-xl")
+
+# Define GPT-2 XL input and output layers
+gpt2_input = gpt2_model.input
+gpt2_output = gpt2_model.output
+
+# Concatenate layers
+concat_layer = layers.Concatenate(name='concat_layer')([lstm_layer, gpt2_output])
+
+# Define output layer
+output_layer = layers.Dense(1, activation='sigmoid', name='output_layer')(concat_layer)
+
+# Define the model
+model = tf.keras.Model(inputs=[text_input, gpt2_input], outputs=output_layer)
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Check if the model file exists, and if so, load the model from the file
+if os.path.exists(CHECKPOINT_FILE):
+    model.load_weights(CHECKPOINT_FILE)
+    print("Loaded model from checkpoint file")
+else:
+    # Fit the model
+    clf = TextClassifier()
+    clf.fit(X_train, y_train)
+    model = clf.export_model()
+    model.save_weights(CHECKPOINT_FILE)
+
+    # Save the processed text data and labels
+    np.save(TEXT_DATA_FILE, X_train)
+    np.save(LABELS_FILE, y_train)
+    
+# Define a function to query the model
+def predict(input_text, num_tokens=10):
+    input_ids = tokenizer.encode(input_text, return_tensors='tf')
+    gpt2_output = gpt2_model.generate(input_ids, max_length=num_tokens, num_return_sequences=1)
+    lstm_output = model.predict([input_ids, gpt2_output])
+    return lstm_output
+
+# Example usage
+output = predict("Do you ever feel like regular queries just don't satisfy you? Well, this GPT-2 and LSTM mashup may just be what you need to spice up your natural language processing game.")
+print(output)
+
+# Reinforcement learning
+for i in range(10):
+    input_text = input("Enter some text: ")
+    expected_output = 1
+    reward = float(input("Enter a reward value(0->1): "))
+    input_ids = tokenizer.encode(input_text, return_tensors='tf')
+    gpt2_output = gpt2_model.generate(input_ids, max_length=10, num_return_sequences=1)
+    lstm_output = model.predict([input_ids, gpt2_output])
+    loss = tf.keras.losses.binary_crossentropy([expected_output], lstm_output)
+    reward = tf.convert_to_tensor([reward])
+
+    with tf.GradientTape() as tape:
+        tape.watch(model.trainable_weights)
+        output = model([input_ids, gpt2_output])
+        gradients = tape.gradient(output, model.trainable_weights)
+
+    for i in range(len(gradients)):
+        if gradients[i] is not None:
+            gradients[i] *= reward
+
+    optimizer = tf.keras.optimizers.Adam()
+    optimizer.apply_gradients(zip(gradients, model.trainable_weights))
