4ff2ed9360439bf969266e912dd2b5d678d98c66
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-04-30 21:37:19 +0200

    Create with_tensorboard_and_dropout_layer.py

---
+++ with_tensorboard_and_dropout_layer.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+import numpy as np
+import os
+import random
+from autokeras import TextClassifier
+from sklearn.model_selection import train_test_split
+
+# Set up file paths
+MODEL_DIR = "path/to/your/model"
+TEXT_DATA_FILE = os.path.join(MODEL_DIR, "text_data.npy")
+GPT2_DATA_FILE = os.path.join(MODEL_DIR, "gpt2_data.npy")
+LABELS_FILE = os.path.join(MODEL_DIR, "labels.npy")
+CHECKPOINT_FILE = os.path.join(MODEL_DIR, "checkpoint.ckpt")
+LOG_FILE = os.path.join(MODEL_DIR, "training.log")
+TENSORBOARD_DIR = os.path.join(MODEL_DIR, "tensorboard")
+
+# Load and preprocess the data, which must be pretokenized
+input_file_path = input("Enter the path/to/your/input/text/file.txt: ")
+with open(input_file_path, 'r') as f:
+    lines = f.readlines()
+text_data = [line.strip() for line in lines]
+labels = [0]*len(text_data)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(text_data, labels, test_size=0.2, random_state=42)
+
+# Define input layer
+text_input = tf.keras.Input(shape=(None,), name='text_input')
+
+# Define LSTM layer
+lstm_layer = layers.LSTM(256, name='lstm_layer')(text_input)
+
+# Define Dropout layer
+dropout_layer = layers.Dropout(0.5, name='dropout_layer')(lstm_layer)
+
+# Select the pre-trained model
+pretrained_model = input("Enter the pre-trained model ('gpt2', 'gpt2-medium', 'gpt2-large' or 'gpt2-xl'): ")
+tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model)
+gpt2_model = TFGPT2LMHeadModel.from_pretrained(pretrained_model)
+
+# Define GPT-2 input and output layers
+gpt2_input = gpt2_model.input
+gpt2_output = gpt2_model.output
+
+# Concatenate layers
+concat_layer = layers.Concatenate(name='concat_layer')([dropout_layer, gpt2_output])
+
+# Define output layer
+output_layer = layers.Dense(1, activation='sigmoid', name='output_layer')(concat_layer)
+
+# Define the model
+model = tf.keras.Model(inputs=[text_input, gpt2_input], outputs=output_layer)
+
+# Compile the model and add model checkpoint callback
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model_checkpoint_callback = ModelCheckpoint(
+    filepath=CHECKPOINT_FILE,
+    save_weights_only=True,
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
+
+# Add tensorboard callback
+tensorboard_callback = TensorBoard(log_dir=TENSORBOARD_DIR, histogram_freq=1)
+
+# Check if the model file exists, and if so, load the model from the file
+if os.path.exists(CHECKPOINT_FILE):
+    model.load_weights(CHECKPOINT_FILE)
+    print("Loaded model from checkpoint file")
+else:
+    # Fit the model
+    clf = TextClassifier()
+    clf.fit(X_train, y_train)
+    model = clf.export_model()
+    model.save_weights(CHECKPOINT_FILE)
+
+    # Save the processed text data and labels
+    np.save(TEXT_DATA_FILE, X_train)
+    np.save(LABELS_FILE, y_train)
+
+# Train the model and save checkpoints at every epoch
+num_epochs = int(input("Enter number of epochs to train: "))
+model.fit(
+    [X_train, gpt2_input], y_train,
+    validation_data=([X_test, gpt2_input], y_test),
+    epochs=num_epochs,
+    batch_size=32,
+    callbacks=[model_checkpoint_callback, tensorboard_callback]
+)
+
+# Define a function to query the model
+def predict(input_text, num_tokens=10):
+    input_ids = tokenizer.encode(input_text, return_tensors='tf')
+    gpt2_output = gpt2_model.generate(input_ids, max_length=num_tokens, num_return_sequences=1)
+    lstm_output = model.predict([input_ids, gpt2_output])
+    return lstm_output
+
+# Example usage
+output = predict("Do you ever feel like regular queries just don't satisfy you? Well, this GPT-2 and LSTM mashup may just be what you need to spice up your natural language processing game.")
+print(output)
+
+# Reinforcement learning
+for i in range(10):
+    input_text = input("Enter some text: ")
+    expected_output = 1
+    reward = float(input("Enter a reward value(0->1): "))
+    input_ids = tokenizer.encode(input_text, return_tensors='tf')
+    gpt2_output = gpt2_model.generate(input_ids, max_length=10, num_return_sequences=1)
+    lstm_output = model.predict([input_ids, gpt2_output])
+    loss = tf.keras.losses.binary_crossentropy([expected_output], lstm_output)
+    reward = tf.convert_to_tensor([reward])
+
+    with tf.GradientTape() as tape:
+        tape.watch(model.trainable_weights)
+        output = model([input_ids, gpt2_output])
+        gradients = tape.gradient(output, model.trainable_weights)
+
+    for i in range(len(gradients)):
+        if gradients[i] is not None:
+            gradients[i] *= reward
+
+    optimizer = tf.keras.optimizers.Adam()
+    optimizer.apply_gradients(zip(gradients, model.trainable_weights))
